ca2cc47d8a959ecbf6d79f251f6903dd1e61358d
Damien Walsh <me@damow.net>
2019-07-21 23:32:46 +0100

    Add: HomeKit functionality

--- src/config-file.ts
+++ src/config-file.ts
@@ -14,6 +14,22 @@ export interface ConnectionConfig {
 }
 
 /**
+ * Supported types of detection zones.
+ */
+export enum ZoneType {
+    Contact = "contact",
+    Motion = "motion"
+}
+
+/**
+ * Configuration for a single zone.
+ */
+export interface ZoneConfig {
+    name: string,
+    type: ZoneType
+}
+
+/**
  * Panel configuration.
  */
 export interface PanelConfig {
@@ -22,15 +38,29 @@ export interface PanelConfig {
 
     // Zones to map
     zones: {
-        [key: number]: {
-            name: string,
-            type: "contact" | "motion"
-        }
+        [key: number]: ZoneConfig
     }
+
+}
+
+/**
+ * HomeKit configuration.
+ */
+export interface HomeKitConfig {
+
+    hap_start_port: number,
+    uuid: string,
+    name: string,
+    manufacturer: string,
+    model: string,
+    serial: string,
+    username: string,
+    pincode: string
 
 }
 
 // Define the shape of the configuration file
 export interface ConfigFile {
+    homekit: HomeKitConfig,
     panel: PanelConfig
 }
--- src/index.ts
+++ src/index.ts
@@ -10,8 +10,12 @@ import * as path from 'path';
 import * as YAML from 'yaml';
 import * as Bunyan from 'bunyan';
 import * as HAPNodeJS from 'hap-nodejs';
-import { ConfigFile } from './config-file';
+import * as storage from 'node-persist'
+import { ConfigFile, ZoneType } from './config-file';
 import { Panel } from './panel';
+
+// Initialise storage
+storage.initSync();
 
 // The config file to use
 const CONFIG_FILE: string = 'config.yml';
@@ -28,7 +32,82 @@ try {
 }
 
 // Create Homekit accessories for the specified zones
-const zoneAccessories: HAPNodeJS.Accessory[] = [];
+const zoneAccessories: { [key: number]: HAPNodeJS.Accessory } = {};
+const zoneStateCache: { [key: number]: boolean } = {};
+let zoneIndex = 0;
+
+for (let zoneNumber in config.panel.zones) {
+    
+    let zone = config.panel.zones[zoneNumber];
+
+    // Create an accessory
+    let accessory = new HAPNodeJS.Accessory(zone.name, HAPNodeJS.uuid.generate(config.homekit.uuid + ':' + zoneIndex));
+    accessory.on('identify', (paired, callback) => {
+        callback();
+    });
+
+    // Add the accessory information service
+    accessory.getService(HAPNodeJS.Service.AccessoryInformation)
+        .setCharacteristic(HAPNodeJS.Characteristic.Manufacturer, config.homekit.manufacturer)
+        .setCharacteristic(HAPNodeJS.Characteristic.Model, config.homekit.model)
+        .setCharacteristic(HAPNodeJS.Characteristic.SerialNumber, config.homekit.serial);
+
+    // Depending on the zone type...
+    switch (zone.type) {
+
+        case ZoneType.Contact:
+        
+            accessory.addService(HAPNodeJS.Service.ContactSensor)
+                .getCharacteristic(HAPNodeJS.Characteristic.ContactSensorState)
+                .on('set', (value, callback) => {
+                    zoneStateCache[zoneNumber] = value ? true : false;
+                    callback();
+                })
+                .on('get', (callback) => {
+                    callback(zoneStateCache[zoneNumber]);
+                });
+
+            break;
+
+        case ZoneType.Motion:
+        
+            accessory.addService(HAPNodeJS.Service.MotionSensor)
+                .getCharacteristic(HAPNodeJS.Characteristic.MotionDetected)
+                .on('set', (value, callback) => {
+                    zoneStateCache[zoneNumber] = value ? true : false;
+                    callback();
+                })
+                .on('get', (callback) => {
+                    callback(zoneStateCache[zoneNumber]);
+                });
+
+            break;
+        
+    }
+
+    // Add the accessory
+    zoneAccessories[zoneNumber] = accessory;
+
+    // Add a state cache entry
+    zoneStateCache[zoneNumber] = false;
+
+    accessory.username = config.homekit.username;
+    accessory.pincode = config.homekit.pincode;
+
+    // Publish the accessory
+    accessory.publish({
+        port: config.homekit.hap_start_port + zoneIndex,
+        username: config.homekit.username,
+        pincode: config.homekit.pincode,
+        category: HAPNodeJS.Accessory.Categories.SENSOR
+    }, true);
+
+    console.info('Publishing accessory', accessory.displayName);
+    zoneIndex ++;
+}
+
+// Output the HomeKit PIN
+console.info(`Use HomeKit PIN: ${config.homekit.pincode}`);
 
 // Create the panel
 const panel = new Panel(config.panel);
